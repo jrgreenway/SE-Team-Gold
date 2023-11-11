ff9aa5e3418ebb1b14c9aec54976ba03e37624fb
Paul-Andrei Petan <pap36@bath.ac.uk>
2023-11-11 22:42:16 +0200

    bug fix

--- buttons/buttonCallbacks.py
+++ buttons/buttonCallbacks.py
@@ -7,18 +7,18 @@ from ast import Tuple
 from screens.screenConstants import CREATE_AVATAR_SCREEN, GAME_SCREEN, LOAD_SCREEN, START_SCREEN, previousScreen
 
 
-def startButtonCB(*_) -> tuple[bool, str]:
+def startButtonCB(**_) -> tuple[bool, str]:
     return True, CREATE_AVATAR_SCREEN
 
-def loadButtonCB(*_) -> tuple[bool, str]:
+def loadButtonCB(**_) -> tuple[bool, str]:
     return True, LOAD_SCREEN
 
-def exitButtonCB(*_) -> tuple[bool, str]:
+def exitButtonCB(**_) -> tuple[bool, str]:
     return False, ''
 
 def backButtonCB(**kwargs) -> tuple[bool, str]:
     currentScreen = kwargs['currentScreen']
     return True, previousScreen(currentScreen)
 
-def startGameButtonCB(*_) -> tuple[bool, str]:
+def startGameButtonCB(**_) -> tuple[bool, str]:
     return True, GAME_SCREEN
