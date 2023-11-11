0af9bc54a8468e18920abe009d0e6eee0a1c23bd
Paul-Andrei Petan <pap36@bath.ac.uk>
2023-11-11 22:04:26 +0200

    created constants for the assets

---
+++ assets/assetsConstants.py
@@ -0,0 +1,6 @@
+CUPBOARD_ASSET = "assets/cupboard.png"
+FLOOR_ASSET = "assets/floor.png"
+OVEN_ASSET = "assets/oven.png"
+SINK_ASSET = "assets/sink.png"
+MALE_CHAR_ASSET = "assets/temp_character_M.jpeg"
+FEMALE_CHAR_ASSET = "assets/temp_character_F.jpeg"
