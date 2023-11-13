3100bf7c8b20461b795d436d56552c202d143eec
sabina0009 <86299521+sabina0009@users.noreply.github.com>
2023-11-13 15:12:47 +0000

    Update player.py
    Added the animate method.

--- player.py
+++ player.py
@@ -29,3 +29,27 @@ class Player:
         based on the current frame and whether the player is moving or not - called on every
         frame in the game logic 
     '''
+
+    #Animate method (returns next sprite image)
+    #Each walking animation in each direction had a 4 frame animation (1st and 3rd being the same image)
+    #Sprite will have 12 images (3 for each direction) named (D1, D2, D3 (walking down), U1, U2, U3, (walking up)...)
+    #Standing image will be D1 (first and 3rd frame of walking down animation)
+
+    def animate(self, moving:bool, currentFrame)：
+        #Arrays of all of the images for the animation
+        walkDown = [pygame.image.load('D1.png'), pygame.image.load('D2.png'), pygame.image.load('D1.png'), pygame.image.load('D3.png')]
+        walkUp = [pygame.image.load('U1.png'), pygame.image.load('U2.png'), pygame.image.load('U1.png'), pygame.image.load('U3.png')]
+        walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R1.png'), pygame.image.load('R3.png')]
+        walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L1.png'), pygame.image.load('L3.png')]
+    
+        if not moving:
+            return walkDown[0]
+        else:
+            if self.facing == "S":
+                return walkDown[(currentFrame//5)%4] #Each image lasts 5 frames so animation loops every 20 frames (maybe be too fast - will have to see when testing)))
+            elif self.facing == "N":
+                return walkUp[(currentFrame//5)%4] 
+            elif self.facing == "E":
+                return walkRight[(currentFrame//5)%4]
+            elif self.facing == "W":
+                return walkLeft[(currentFrame//5)%4]
